019a05813f072548aa058a2ba62b513a89baf2ae
anton <l2u7ck@gmail.com>
2024-07-10 15:31:49 +0300

    First task

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
+from models import create_table
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    DSN = 'postgresql://postgres:1324@localhost:5432/database_ORM_python'
+    engine = sqlalchemy.create_engine(DSN)
+
+    create_table(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.close()
---
+++ models.py
@@ -0,0 +1,53 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=100), unique=True)
+    publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
+
+
+class Shop(Base):
+    __tablename__ = "shop"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
+    shop_id = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+
+class Sale(Base):
+    __tablename__ = "sale"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Integer, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+
+def create_table(engine):
+    Base.metadata.create_all(engine)
+
+
+
+
